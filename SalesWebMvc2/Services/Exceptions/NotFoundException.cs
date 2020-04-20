1502525e44c4e30c24f89393f728d83fb3431bb4
DELL <DELL@DESKTOP-8IOGNVP>
2020-04-19 23:47:46 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc2/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMvc2.Services.Exceptions
+{
+    public class NotFoundException: ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
