4365466f56f5ffcd3621f94b116a13875b8ae50b
DELL <DELL@DESKTOP-8IOGNVP>
2020-04-25 02:08:24 -0300

    Implementing grouping search

--- SalesWebMvc2/Models/SalesRecord.cs
+++ SalesWebMvc2/Models/SalesRecord.cs
@@ -7,7 +7,11 @@ namespace SalesWebMvc2.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd:MM:yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0}:F2")]
         public Double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
